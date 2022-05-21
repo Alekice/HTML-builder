d8f39a9d34abc98052325b6db8c9c5e83df27d52
Aleksandra <semenovalekshu@gmail.com>
2022-05-21 10:05:10 +0300

    refactor: change some code 01-read-file

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
-const process = require('process');
-const content = fs.createReadStream(path.join(__dirname, 'text.txt'), 'utf-8');
-content.on('data', data => process.stdout.write(data.trim()));
+const { stdout } = require('process');
+
+const input = fs.createReadStream(path.join(__dirname, 'text.txt'), 'utf-8');
+input.on('data', chunk => stdout.write(chunk));
